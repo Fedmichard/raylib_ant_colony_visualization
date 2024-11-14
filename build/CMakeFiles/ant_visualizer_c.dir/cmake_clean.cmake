0902da265500f30217d6c007e8b921ef37da7a8d
Fedmichard <francoisfedmichard@gmail.com>
2024-11-14 10:30:52 -0500

    New Project Initialized

---
+++ build/CMakeFiles/ant_visualizer_c.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ant_visualizer_c.dir/src/main.c.obj"
+  "CMakeFiles/ant_visualizer_c.dir/src/main.c.obj.d"
+  "ant_visualizer_c.exe"
+  "ant_visualizer_c.exe.manifest"
+  "ant_visualizer_c.pdb"
+  "libant_visualizer_c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ant_visualizer_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+#define WIDTH 800
+#define HEIGHT 600
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+// Compile with GCC Compiler
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+
+    InitWindow(WIDTH, HEIGHT, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Updates / Inputs
+        //----------------------------------------------------------------------------------
+
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+            GuiPanel((Rectangle){ 675, 100, 100, 25 }, "Hi!");
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
